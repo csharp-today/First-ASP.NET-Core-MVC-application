2b80b5ab9d3df3d38c986344b024f180297093b2
mariusz <mariusz@MONSTER>
2017-01-23 00:10:20 +0100

    Building first ASP.NET Core MVC application

---
+++ First-ASP.NET-Core-MVC-application/src/First-ASP.NET-Core-MVC-application/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace First_ASP.NET_Core_MVC_application.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home/index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ First-ASP.NET-Core-MVC-application/src/First-ASP.NET-Core-MVC-application/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace First_ASP.NET_Core_MVC_application
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
